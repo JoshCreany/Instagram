ebd8c0264ff620995f1f6da267cf2d69a7e6ad01
JoshCreany <joshcreany@gmail.com>
2020-07-13 20:38:10 -0600

    copied all things from post

---
+++ Instagram/Instagram.Android/Views/Instagramviewpage.cs
@@ -0,0 +1,118 @@
+﻿using Xamarin.Forms;
+using XamarinForms.ViewModels;
+
+namespace XamarinForms.Views
+{
+    public class InstagramViewPage : ContentPage
+    {
+
+        public InstagramViewPage()
+        {
+
+            BackgroundColor = Color.White;
+
+            var twitterViewModel = new InstagramViewModel();
+
+            BindingContext = twitterViewModel;
+
+            var label = new Label
+            {
+                Text = "Instagram",
+                TextColor = Color.Gray,
+                FontSize = 24
+            };
+
+            var dataTemplate = new DataTemplate(() =>
+            {
+                var screenNameLabel = new Label
+                {
+                    TextColor = Color.FromHex("#1c5380"),
+                    FontSize = 22
+                };
+                var textLabel = new Label
+                {
+                    TextColor = Color.Black,
+                    FontSize = 14
+                };
+                var likesLabel = new Label
+                {
+                    TextColor = Color.FromHex("#517fa4"),
+                    FontSize = 14
+                };
+                var commentsLabel = new Label
+                {
+                    TextColor = Color.FromHex("#517fa4"),
+                    FontSize = 14
+                };
+                var profileImage = new Image
+                {
+                    WidthRequest = 60,
+                    HeightRequest = 60,
+                    VerticalOptions = LayoutOptions.Start
+                };
+                var mediaImage = new Image
+                {
+                    HeightRequest = 200
+                };
+
+                screenNameLabel.SetBinding(Label.TextProperty, new Binding("UserName"));
+                textLabel.SetBinding(Label.TextProperty, new Binding("Text"));
+                profileImage.SetBinding(Image.SourceProperty, new Binding("ProfilePicture"));
+                mediaImage.SetBinding(Image.SourceProperty, new Binding("StandardResolutionUrl"));
+                likesLabel.SetBinding(Label.TextProperty, new Binding("LikesCount", BindingMode.Default, null, null, "{0:n0} likes |"));
+                commentsLabel.SetBinding(Label.TextProperty, new Binding("CommentsCount", BindingMode.Default, null, null, "{0:n0} likes"));
+
+                return new ViewCell
+                {
+                    View = new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Padding = new Thickness(0, 5),
+                        Children =
+                        {
+                            profileImage,
+                            new StackLayout
+                            {
+                                Orientation = StackOrientation.Vertical,
+                                Children =
+                                {
+                                    screenNameLabel,
+                                    new StackLayout
+                                    {
+                                        Orientation = StackOrientation.Horizontal,
+                                        Children =
+                                        {
+                                            likesLabel,
+                                            commentsLabel,
+                                        }
+                                    },
+                                    textLabel,
+                                    mediaImage,
+                                }
+                            }
+                        }
+                    }
+                };
+            });
+
+            var listView = new ListView
+            {
+                HasUnevenRows = true
+            };
+
+            listView.SetBinding(ListView.ItemsSourceProperty, "InstagramItems");
+
+            listView.ItemTemplate = dataTemplate;
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(5, 10),
+                Children =
+                {
+                    label,
+                    listView
+                }
+            };
+        }
+    }
+}
---
+++ Instagram/Instagram/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XamarinForms.Views;
+
+using XamarinForms.Models;
+using XamarinForms.ViewModels;
+
+namespace Instagram
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new InstagramViewPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts  
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps  
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes  
+        }
+    }
+}
